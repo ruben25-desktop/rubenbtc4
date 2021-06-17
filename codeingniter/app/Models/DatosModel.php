17958ed36b9d26f7fef1be2a489e7934974b1bc7
ruben25-desktop <58815346+ruben25-desktop@users.noreply.github.com>
2021-06-16 21:24:34 -0500

    Update DatosModel.php

--- codeingniter/app/Models/DatosModel.php
+++ codeingniter/app/Models/DatosModel.php
@@ -1,1 +1,14 @@
-
+<?php
+namespace App\Models;
+use Codeigniter\Model;
+class GeneralModel extends Model{
+  Public function obtenerInformacion($data){
+    $gModel = $this->db->table('persona');
+    $gModel->where($data);
+    return $gmodel->get()->getResultArray();
+    
+  }
+  
+  public function listarTodo(){
+    $gModel = $this->db->query("SELECT * FROM persona");
+    return $gModel->getresult();
